938bff9cb1b5036d4206f838c64f28d6ac82f022
solomon-ayodele65 <solomonayodele65@gmail.com>
2021-02-22 12:30:06 +0100

    new kairo market

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
+
+import user from '../../images/user.jpg'
 
 export class Sidebar extends Component {
     render() {
         return (
             <div>
                 <div className="sidebar">
-                    <div className="">
-                        
-                    </div>
+                    <br/>
+                          <div className="user-profile-pic mt-5">
+                            <img src={user} alt="user" width="200px" height="150px" className="rounded-circle"/>
+                          </div>
+                                <Link class="active" to="/dashboard">Dashboard</Link>
+                                <Link to="/profile">Profile</Link>
+                                <Link to="/product">Products</Link>
+                           
                 </div>
             </div>
         )
--- src/pages/Users/Dashboard.js
+++ src/pages/Users/Dashboard.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import { Link,Router,Route,Switch } from 'react-router-dom'
 
+import Sidebar from '../../components/Sidebar/Sidebar'
+
 function Dashboard() {
     return (
         <div>
             <div className="dashboard">
-
+            {/* <Sidebar/> */}
+                <div className="row">
+                    <div className="col-md-3">
+                       <Sidebar/>
+                    </div>
+                    <div className="col-md-8">
+                    <h1> lorem ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                       Lorem Ipsum has been the industry's standard dummy text ever since the
+                       1500s, when an unknown printer took a galley of type and scrambled it to
+                       make a type specimen book. It has survived not only five centuries, but
+                       also the leap into electronic typesetting, remaining essentially
+                       unchanged. It was popularised in the 1960s with the release of Letraset
+                       sheets containing Lorem Ipsum passages, and more recently with desktop
+                       publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h1>
+                    </div>
+                </div>
             </div>
         </div>
     )
