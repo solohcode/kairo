6328d604c7090857a3e8f2bd318c13a5da793866
benmat <tawakaltadisa99@gmail.com>
2021-02-24 11:10:26 +0100

    tawa

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 function Login() {
     return (
         <div>
-            <h1>Login page</h1>
+            <br/>
+            <div className="container" style={{width:"80%", backgroundColor:"#a9a9a9",marginTop:"50px",marginBottom:"50px"}}>
+        <h5 style={{textAlign:"center", color:"red",marginBottom:"15px"}}>
+            Not a member yet? 
+        </h5>
+            <div class="card" style={{height:"500px",paddingBottom:30}}>
+                <div className="card-body">
+                    This is some text within a card body.
+                </div>
+</div>
+            </div>
         </div>
     )
 }
