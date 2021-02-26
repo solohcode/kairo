4220548e73a1b30e128791ff4ba5f2379c51804c
solomon-ayodele65 <solomonayodele65@gmail.com>
2021-02-26 11:28:34 +0100

    updates

--- src/pages/Auth/Register.js
+++ src/pages/Auth/Register.js
@@ -3,7 +3,9 @@ import React from 'react'
 function Register() {
     return (
         <div>
-            <div></div>
+            <div>
+                
+            </div>
         </div>
     )
 }
